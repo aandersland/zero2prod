42daf1d9ca33f5a39f8da9f675d07a90b560087f
aandersland <a.andersland@gmail.com>
2024-07-08 15:46:01 -0500

    fix: init db script

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -25,16 +25,22 @@ DB_HOST="${POSTGRES_HOST:=localhost}"
 
 if [[ -z "${SKIP_DOCKER}" ]]
 then
+    RUNNING_POSTGRES_CONTAINER=$(docker ps --filter 'name=postgres' --format '{{.ID}}')
+    if [[ -n $RUNNING_POSTGRES_CONTAINER]]; then
+        echo >&2  "there is a runing postgres container already running, kill it with"
+        echo >&2 "  docker kill ${RUNNING_POSTGRES_CONTAINER}"
+        exit 1
+    fi
     docker run \
         -e POSTGRES_USER=${DB_USER} \
         -e POSTGRES_PASSWORD=${DB_PASSWORD} \
         -e POSTGRES_DB=${DB_NAME} \
         -p "${DB_PORT}":5432 \
-        -d postgres \
+        -d \
+        --name "postgres_$(date '+%s')" \
         postgres -N 1000
 fi
-export PGPASSWORD="${DB_PASSWORD}"
-until psql -h "${DB_HOST}" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q'; do
+until PGPASSWORD="${DB_PASSWORD}" psql -h "${DB_HOST}" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q'; do
     >&2 echo "Postgres is still unavailable - sleeping"
     sleep 1
 done
